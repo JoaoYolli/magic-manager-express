d05bb07feed8c32c93571c086dc0fb1d4b2ce2f0
JoaoYolli <yoaojoao@gmail.com>
2024-09-21 00:54:28 +0200

    Seeking server issues

--- email.js
+++ email.js
@@ -1,48 +1,53 @@
 var nodemailer = require('nodemailer');
-let code = "" 
+let code = ""
 require('dotenv').config();
 
-function sendVerificationMail(mail){
-  return new Promise((resolve) =>{
-
-    let uuid = crypto.randomUUID();
+function sendVerificationMail(mail) {
+  return new Promise((resolve) => {
+    try {
+      let uuid = crypto.randomUUID();
     uuid = uuid.split("-");
     code = uuid[0];
-  
-    var transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-          user: process.env.MAIL,
-          pass: process.env.MAIL_PASS
-        }
-      });
-      
-      var mailOptions = {
-        from: 'noreply@yolliservices.com',
-        to: mail,
-        subject: 'Verify email',
-        text: `Your code to log in to Magic Manager : ${uuid[0]} \nIf you are not trying to log in just ignore this mail, It's safe :)`
-      };
-      
-      transporter.sendMail(mailOptions, function(error, info){
-        if (error) {
-          console.log(error);
-        } else {
-          console.log('Email sent: ' + info.response);
-        }
-      });
-      resolve(code)
-  })
-    
-}
 
-function verifyCode(cod){
-    if(cod == code){
-        return true;
-    }else{
-        return false;
+    var transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: process.env.MAIL,
+        pass: process.env.MAIL_PASS
+      }
+    });
+
+    var mailOptions = {
+      from: process.env.MAIL,
+      to: mail,
+      subject: 'Verify email',
+      text: `Your code to log in to Magic Manager : ${uuid[0]} \nIf you are not trying to log in just ignore this mail, It's safe :)`
+    };
+
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('Email sent: ' + info.response);
+      }
+    });
+    resolve(code)
+    } catch (error) {
+      console.log(error)
+      resolve(code)
     }
     
+  })
+
 }
 
-module.exports = {sendVerificationMail, verifyCode}
+function verifyCode(cod) {
+  if (cod == code) {
+    return true;
+  } else {
+    return false;
+  }
+
+}
+
+module.exports = { sendVerificationMail, verifyCode }
